b0b7c64130c41c589126906e253ae8d0da67169e
Vaibhav Satish <vasatish@uci.edu>
2025-04-12 16:17:36 -0700

    commiting

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import pandas as pd
 from pathlib import Path
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
 
-DATA_FILE = Path('Data/data.csv')
+
+DATA_FILE = Path('data.csv')
 data_frame = None
 
 def clean_data() -> pd.DataFrame:
@@ -29,20 +28,13 @@ class NeuralNetwork(nn.Module):
         self.train_data, self.test_data = train_test_split(self._data, train_size=0.80, test_size=0.20, random_state=42)
         
     def run(self):
-        pass
+        print('here')
 
 # so do we just run chatgpt (chatgpt api is the  way ) and nmake it sovle it????? --yes -- thats goofy -- that's cs
 # w can import decision tree
 # instead of decision tree cant we just use scikit to do th esame things as pytorch-Neural Network?? yea
 
 
-
-class DecisionTree(DecisionTreeClassifier):
-    def __init__(self, data):
-        self._data = data
-    
-    def determine_output(self):
-        pass 
     
 if __name__ == '__main__':
     data = clean_data()
